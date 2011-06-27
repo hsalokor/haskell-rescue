a45ffa2c5d87f91f70b6de39e9d3008d2268da44
Juha Paananen <juha.paananen@gmail.com>
2011-06-27 18:40:22 +0300

    add threshold to logger

--- 2011-06/logger.hs
+++ 2011-06/logger.hs
@@ -1,4 +1,10 @@
+import Control.Monad(when)
+
 data Logger = Logger { info :: Log, warn :: Log, error :: Log }
+data Level = Info|Warn|Error deriving (Show, Ord, Eq)
 type Log = String -> IO ()
-sysOutLogger = Logger (log "Info") (log "Warn") (log "Error")
-  where log level msg = putStrLn $ level ++ " - " ++ msg
+
+sysOutLogger threshold = Logger (log Info) (log Warn) (log Error)
+  where log level msg = when (level >= threshold) $ putStrLn $ (show level) ++ " - " ++ msg
+
+
